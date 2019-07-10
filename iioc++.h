2671920dad4c9be250983a4ff2aae677e3320a02
vs42 <shubinvalentin@gmail.com>
2019-07-10 18:08:46 +0300

    Signed-off-by: vs42 <shubinvalentin@gmail.com>

--- iioc++.h
+++ iioc++.h
@@ -7,6 +7,11 @@
 class Buffer;
 class Device;
 class Context;
+class Channel;
+class Device_Attribute;
+class Channel_Attribute;
+class Device_Attributes;
+class Channel_Attributes;
 class Context_Devices;
 
 class Buffer {
@@ -78,14 +83,47 @@ public:
     }
 };
 
+class Device_Attribute {
+    Device *dev;
+public:
+    std::string key;
+    Device_Attribute (std::string str, Device *device) {
+        key = str;
+        dev = device;
+    }
+    Device_Attribute& operator =(std::string str);
+    Device_Attribute& operator =(long long str);
+    Device_Attribute& operator =(double str);
+    Device_Attribute& operator =(bool str);
+    Device_Attribute& operator =(Device_Attribute d);
+    bool operator ==(Device_Attribute d);
+    std::string value();
+};
+
+class Device_Attributes {
+    Device* a;
+public:
+    Device_Attributes(Device* b) {
+        a = b;
+    }
+    int size();
+    std::string operator[] (unsigned int i);
+    Device_Attribute operator[] (std::string& s);
+};
+
+
 class Device {
     iio_device *dev;
 public:
-    Device(iio_device* device) {
+    friend Device_Attributes;
+    friend Device_Attribute;
+    Device_Attributes attributes;
+    Device(iio_device* device) : attributes(this) {
         dev = device;
     }
     std::string id();
     std::string name();
+    Channel* find_channel(std::string s, bool output);
 };
 
 class Context_Devices {
@@ -95,8 +133,8 @@ public:
         a = b;
     }
     int size();
-    Device operator[] (unsigned int i);
-    Device operator[] (std::string s);
+    Device* operator[] (unsigned int i);
+    Device* operator[] (std::string s);
 };
 
 class Context {
@@ -104,15 +142,16 @@ class Context {
 public:
     Context_Devices devices;
     friend Context_Devices;
-    Context(iio_context* con): c(this) {
+    friend Device;
+    Context(iio_context* con): devices(this) {
         a = con;
     }
 
-    Context(): c(this) {
+    Context(): devices(this) {
         a = iio_create_default_context();
     }
 
-    Context(std::string type, std::string s = ""): c(this) {
+    Context(std::string type, std::string s = ""): devices(this) {
         if (type == "uri") {
             a = iio_create_context_from_uri(s.c_str());
         } else if (type == "local") {
@@ -124,7 +163,7 @@ public:
         }
     }
 
-    Context(const Context& c): c(this) {
+    Context(const Context& c): devices(this) {
         a = iio_context_clone(c.a);
     }
 
@@ -148,6 +187,61 @@ public:
     }
 };
 
+class Channel_Attribute {
+    Channel *dev;
+public:
+    std::string key;
+    Channel_Attribute (std::string str, Channel *device) {
+        key = str;
+        dev = device;
+    }
+    Channel_Attribute& operator =(std::string str);
+    Channel_Attribute& operator =(long long str);
+    Channel_Attribute& operator =(double str);
+    Channel_Attribute& operator =(bool str);
+    Channel_Attribute& operator =(Channel_Attribute d);
+    bool operator ==(Channel_Attribute d);
+    std::string value();
+};
+
+class Channel_Attributes {
+    Channel* a;
+public:
+    Channel_Attributes(Channel* b) {
+        a = b;
+    }
+    int size();
+    std::string operator[] (unsigned int i);
+    Channel_Attribute operator[] (std::string s);
+};
+
+class Channel {
+    iio_channel *a;
+public:
+    friend Channel_Attributes;
+    friend Channel_Attribute;
+    Channel_Attributes attributes;
+
+    Channel (iio_channel *b) : attributes(this) {
+        a = b;
+    }
+
+    std::string name() {
+        return std::string(iio_channel_get_name(a));
+    }
+
+    std::string id() {
+        return std::string(iio_channel_get_id(a));
+    }
+
+    void disable() {
+        iio_channel_disable(a);
+    }
+    ~Channel() {
+        this->disable();
+    }
+};
+
 std::string Device::id() {
     return std::string(iio_device_get_id(dev));
 }
@@ -160,10 +254,97 @@ int Context_Devices::size() {
     return iio_context_get_devices_count(a->a);
 }
 
-Device Context_Devices::operator[] (unsigned int i) {
-    return iio_context_get_device(a->a, i);
+Device* Context_Devices::operator[] (unsigned int i) {
+    auto d = new Device(iio_context_get_device(a->a, i));
+    return d;
 }
 
-Device Context_Devices::operator[] (std::string s) {
-    return iio_context_find_device(a->a, s.c_str());
+Device* Context_Devices::operator[] (std::string s) {
+    auto d = new Device(iio_context_find_device(a->a, s.c_str()));
+    return d;
+}
+
+std::string Channel_Attributes::operator[] (unsigned int i) {
+    return std::string(iio_channel_get_attr(a->a, i));
+}
+
+Channel_Attribute Channel_Attributes::operator[] (std::string s) {
+    return Channel_Attribute(s, a);
+}
+
+std::string Device_Attributes::operator[] (unsigned int i) {
+    return std::string(iio_device_get_attr(a->dev, i));
+}
+
+Device_Attribute Device_Attributes::operator[] (std::string& s) {
+    return Device_Attribute(s, a);
+}
+
+Channel* Device::find_channel(std::string s, bool output) {
+    auto c = new Channel(iio_device_find_channel(dev, s.c_str(), output));
+    return c;
+}
+
+Device_Attribute& Device_Attribute::operator =(std::string str) {
+    iio_device_attr_write(dev->dev, key.c_str(), str.c_str());
+    return *this;
+}
+
+Device_Attribute& Device_Attribute::operator = (long long str){
+    iio_device_attr_write_longlong(dev->dev, key.c_str(), str);
+    return *this;
+}
+
+Device_Attribute& Device_Attribute::operator = (bool str){
+    iio_device_attr_write_bool(dev->dev, key.c_str(), str);
+    return *this;
+}
+
+Device_Attribute& Device_Attribute::operator = (double str){
+    iio_device_attr_write_double(dev->dev, key.c_str(), str);
+    return *this;
+}
+
+Device_Attribute& Device_Attribute::operator =(Device_Attribute d) {
+    iio_device_attr_write(dev->dev, key.c_str(), iio_device_find_attr(d.dev->dev, d.key.c_str()));
+    return *this;
+}
+bool Device_Attribute::operator ==(Device_Attribute d) {
+    return iio_device_find_attr(dev->dev, key.c_str()) == iio_device_find_attr(d.dev->dev, d.key.c_str());
+}
+
+std::string Device_Attribute::value() {
+    return std::string(iio_device_find_attr(dev->dev, key.c_str()));
+}
+
+Channel_Attribute& Channel_Attribute::operator =(std::string str) {
+    iio_channel_attr_write(dev->a, key.c_str(), str.c_str());
+    return *this;
+}
+
+Channel_Attribute& Channel_Attribute::operator = (long long str){
+    iio_channel_attr_write_longlong(dev->a, key.c_str(), str);
+    return *this;
+}
+
+Channel_Attribute& Channel_Attribute::operator = (bool str){
+    iio_channel_attr_write_bool(dev->a, key.c_str(), str);
+    return *this;
+}
+
+Channel_Attribute& Channel_Attribute::operator = (double str){
+    iio_channel_attr_write_double(dev->a, key.c_str(), str);
+    return *this;
+}
+
+Channel_Attribute& Channel_Attribute::operator =(Channel_Attribute d) {
+    iio_channel_attr_write(dev->a, key.c_str(), iio_channel_find_attr(d.dev->a, d.key.c_str()));
+    return *this;
+}
+bool Channel_Attribute::operator ==(Channel_Attribute d) {
+    return iio_channel_find_attr(dev->a, key.c_str()) == iio_channel_find_attr(d.dev->a, d.key.c_str());
+}
+
+std::string Channel_Attribute::value() {
+    return std::string(iio_channel_find_attr(dev->a, key.c_str()));
 }
--- test.cpp
+++ test.cpp
@@ -57,11 +57,11 @@ struct stream_cfg {
 static char tmpstr[64];
 
 /* IIO structs required for streaming */
-static struct iio_context *ctx   = NULL;
-static struct iio_channel *rx0_i = NULL;
-static struct iio_channel *rx0_q = NULL;
-static struct iio_channel *tx0_i = NULL;
-static struct iio_channel *tx0_q = NULL;
+Context *ctx   = NULL;
+Channel *rx0_i = NULL;
+Channel *rx0_q = NULL;
+Channel *tx0_i = NULL;
+Channel *tx0_q = NULL;
 Buffer  *rxbuf = NULL;
 Buffer  *txbuf = NULL;
 
@@ -74,13 +74,13 @@ static void shutdown()
 	rxbuf->destroy();
 	txbuf->destroy();
 	printf("* Disabling streaming channels\n");
-	if (rx0_i) { iio_channel_disable(rx0_i); }
-	if (rx0_q) { iio_channel_disable(rx0_q); }
-	if (tx0_i) { iio_channel_disable(tx0_i); }
-	if (tx0_q) { iio_channel_disable(tx0_q); }
+	rx0_i->disable();
+	rx0_q->disable();
+	tx0_i->disable();
+	tx0_q->disable();
 
 	printf("* Destroying context\n");
-	if (ctx) { iio_context_destroy(ctx); }
+	ctx->destroy();
 	exit(0);
 }
 
@@ -96,15 +96,15 @@ static void errchk(int v, const char* what) {
 }
 
 /* write attribute: long long int */
-static void wr_ch_lli(struct iio_channel *chn, const char* what, long long val)
+static void wr_ch_lli(Channel *chn, const char* what, long long val)
 {
-	errchk(iio_channel_attr_write_longlong(chn, what, val), what);
+	chn->attributes[what] = val;
 }
 
 /* write attribute: string */
-static void wr_ch_str(struct iio_channel *chn, const char* what, const char* str)
+static void wr_ch_str(Channel *chn, const char* what, const char* str)
 {
-	errchk(iio_channel_attr_write(chn, what, str), what);
+	chn->attributes[what] =  str;
 }
 
 /* helper function generating channel names */
@@ -115,25 +115,24 @@ static char* get_ch_name(const char* type, int id)
 }
 
 /* returns ad9361 phy device */
-static struct iio_device* get_ad9361_phy(struct iio_context *ctx)
+Device* get_ad9361_phy(Context *ctx)
 {
-	struct iio_device *dev =  iio_context_find_device(ctx, "ad9361-phy");
-	ASSERT(dev && "No ad9361-phy found");
+	Device *dev =  ctx->devices["ad9361-phy"];
 	return dev;
 }
 
 /* finds AD9361 streaming IIO devices */
-static bool get_ad9361_stream_dev(struct iio_context *ctx, enum iodev d, struct iio_device **dev)
+static bool get_ad9361_stream_dev(Context *ctx, enum iodev d, Device **dev)
 {
 	switch (d) {
-	case TX: *dev = iio_context_find_device(ctx, "cf-ad9361-dds-core-lpc"); return *dev != NULL;
-	case RX: *dev = iio_context_find_device(ctx, "cf-ad9361-lpc");  return *dev != NULL;
+	case TX: *dev = ctx->devices["cf-ad9361-dds-core-lpc"]; return *dev != NULL;
+	case RX: *dev = ctx->devices["cf-ad9361-lpc"];  return *dev != NULL;
 	default: ASSERT(0); return false;
 	}
 }
 
 /* finds AD9361 streaming IIO channels */
-static bool get_ad9361_stream_ch(struct iio_context *ctx, enum iodev d, struct iio_device *dev, int chid, struct iio_channel **chn)
+static bool get_ad9361_stream_ch(Context *ctx, enum iodev d, Device *dev, int chid, Channel **chn)
 {
 	*chn = iio_device_find_channel(dev, get_ch_name("voltage", chid), d == TX);
 	if (!*chn)
@@ -142,30 +141,30 @@ static bool get_ad9361_stream_ch(struct iio_context *ctx, enum iodev d, struct i
 }
 
 /* finds AD9361 phy IIO configuration channel with id chid */
-static bool get_phy_chan(struct iio_context *ctx, enum iodev d, int chid, struct iio_channel **chn)
+static bool get_phy_chan(Context *ctx, enum iodev d, int chid, Channel **chn)
 {
 	switch (d) {
-	case RX: *chn = iio_device_find_channel(get_ad9361_phy(ctx), get_ch_name("voltage", chid), false); return *chn != NULL;
-	case TX: *chn = iio_device_find_channel(get_ad9361_phy(ctx), get_ch_name("voltage", chid), true);  return *chn != NULL;
+	case RX: *chn = get_ad9361_phy(ctx)->find_channel(get_ch_name("voltage", chid), false); return *chn != NULL;
+	case TX: *chn = get_ad9361_phy(ctx)->find_channel(get_ch_name("voltage", chid), true);  return *chn != NULL;
 	default: ASSERT(0); return false;
 	}
 }
 
 /* finds AD9361 local oscillator IIO configuration channels */
-static bool get_lo_chan(struct iio_context *ctx, enum iodev d, struct iio_channel **chn)
+static bool get_lo_chan(Context *ctx, enum iodev d, Channel **chn)
 {
 	switch (d) {
 	 // LO chan is always output, i.e. true
-	case RX: *chn = iio_device_find_channel(get_ad9361_phy(ctx), get_ch_name("altvoltage", 0), true); return *chn != NULL;
-	case TX: *chn = iio_device_find_channel(get_ad9361_phy(ctx), get_ch_name("altvoltage", 1), true); return *chn != NULL;
+	case RX: *chn = get_ad9361_phy(ctx)->find_channel(get_ch_name("altvoltage", 0), true); return *chn != NULL;
+	case TX: *chn = get_ad9361_phy(ctx)->find_channel(get_ch_name("altvoltage", 1), true); return *chn != NULL;
 	default: ASSERT(0); return false;
 	}
 }
 
 /* applies streaming configuration through IIO */
-bool cfg_ad9361_streaming_ch(struct iio_context *ctx, struct stream_cfg *cfg, enum iodev type, int chid)
+bool cfg_ad9361_streaming_ch(Context *ctx, struct stream_cfg *cfg, enum iodev type, int chid)
 {
-	struct iio_channel *chn = NULL;
+	Channel *chn = NULL;
 
 	// Configure phy and lo channels
 	printf("* Acquiring AD9361 phy channel %d\n", chid);
@@ -186,8 +185,8 @@ bool cfg_ad9361_streaming_ch(struct iio_context *ctx, struct stream_cfg *cfg, en
 int main (int argc, char **argv)
 {
 	// Streaming devices
-	struct iio_device *tx;
-	struct iio_device *rx;
+	Device *tx;
+	Device *rx;
 
 	// RX and TX sample counters
 	size_t nrx = 0;
@@ -215,8 +214,8 @@ int main (int argc, char **argv)
 	txcfg.rfport = "A"; // port A (select for rf freq.)
 
 	printf("* Acquiring IIO context\n");
-	ASSERT((ctx = iio_create_network_context("192.168.2.1")) && "No context");
-	ASSERT(iio_context_get_devices_count(ctx) > 0 && "No devices");
+	ctx = new Context("network", "192.168.2.1");
+	ASSERT(ctx->devices.size() > 0 && "No devices");
 
 	printf("* Acquiring AD9361 streaming devices\n");
 	ASSERT(get_ad9361_stream_dev(ctx, TX, &tx) && "No tx dev found");
